816528d2b0b572936161d897b4d3d8169266cb8c
vedanthaha <107772725+vedanthaha@users.noreply.github.com>
2025-06-30 00:47:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-const cors = require('cors');
 app.use(cors({
     origin: [
         'https://lovely-unicorn-14a79e.netlify.app/', // your Netlify site
